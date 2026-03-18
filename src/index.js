663c59df3b5768d66e2ddd2d551a8903c12f397b
RaghavMaheshwari2024 <raghavmaheshwari.2028@gmail.com>
2026-03-18 19:33:12 +0530

    add retryWithBackoff test and update gitignore

--- src/index.js
+++ src/index.js
@@ -39,4 +39,11 @@ const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=>{
     console.log('Server is running on port', PORT);
-});                                                         
+});                  
+
+
+if (require.main === module) {
+  app.listen(3000, () => console.log('Server running'));
+}
+
+module.exports = app;
